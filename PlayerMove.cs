b560926c12f279036112e810b56adb9e61476c0c
Sanyaa-0 <165273453+Sanyaa-0@users.noreply.github.com>
2024-03-29 10:26:00 +0100

    Add files via upload

---
+++ PlayerMove.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+
+{
+    public.float speed = 1;
+    public.float jumpforce = 5;
+
+    Rigidbode2D rb;
+    SpriteRenderer sr;
+
+    void Start()
+    {
+        rb = GetComponent <Rigidbody2D>();
+        sr = GetComponent <SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * speed * Time.deltaTime
+
+            if (input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.05f) 
+            rb.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+
+        sr.flipX = movement < 0 ? true : false;
+    }
+}
